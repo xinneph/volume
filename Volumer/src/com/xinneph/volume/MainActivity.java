601234163340b06bc4baf94b1b7987e7382f4c7e
xinneph <xinneph@gmail.com>
2014-07-02 20:33:21 +0200

    parsing the page content

--- Volumer/src/com/xinneph/volume/MainActivity.java
+++ Volumer/src/com/xinneph/volume/MainActivity.java
@@ -1,16 +1,31 @@
 package com.xinneph.volume;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Activity;
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
+    public static final String TAG = "Volumer";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +36,7 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        new DownloadDataTask().execute();
     }
 
 
@@ -55,9 +71,92 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.volume_and_pips, container, false);
             return rootView;
         }
     }
+    
+    
+    public class DownloadDataTask extends AsyncTask<Void,Void,String> {
+        @Override
+        protected String doInBackground(Void...voids) {
+            String website = "http://nbp.pl/link/kursy/tabela-a";
+//            String website = "http://nbp.pl/kursy/xml/a125z140701.xml";
+            try {
+                URL url = new URL(website);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                InputStream stream = conn.getInputStream();
+                stream.skip("<!doctype html>".length());
 
+                PageParser pp = new PageParser();
+                String xmlPath = pp.parse(stream);
+                Log.v(TAG, "xmlPath="+xmlPath);
+                return xmlPath;
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "wrong url given", e);
+            } catch (IOException e) {
+                Log.e(TAG, "error when opening http connection", e);
+            }
+            return website;
+        }
+        
+        @Override
+        protected void onPostExecute(String result) {
+//            setContentView(R.layout.web);
+//            WebView webView = (WebView) findViewById(R.id.webView);
+//            webView.loadData(result, "text/html", null);
+        }
+        
+    }
+    
+    public static class PageParser {
+        private static final String ns = null;
+        
+        public String parse(InputStream in) throws IOException {
+            try {
+                XmlPullParser parser = Xml.newPullParser();
+                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+                parser.setInput(in, null);
+//                parser.nextTag();
+//                parser.next();
+                String str = "Powyższa tabela w formacie .xml";
+                String href = null;
+                while (true) {
+                    try {
+                        if (parser.nextToken() == XmlPullParser.END_DOCUMENT)
+                            return null;
+                        int event = parser.getEventType();
+                        if (event == XmlPullParser.TEXT) {
+                            String text = parser.getText();
+                            if (text.equals(str))
+                                return href;
+                        }
+                        else if (event == XmlPullParser.START_TAG) {
+
+                            String name = parser.getName();
+
+                            if (name.equals("a")) {
+                                href = parser.getAttributeValue(ns, "href");
+                            }
+                        }
+                    }
+                    catch (XmlPullParserException e) {
+//                        Log.w(TAG, "error when parsing", e);
+                    }
+                }
+            }
+            catch (XmlPullParserException e) {
+                Log.w(TAG, "error when parsing", e);
+            }
+            finally {
+                in.close();
+            }
+            return null;
+        }
+    }
 }
